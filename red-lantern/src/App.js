18f4540c7fbc88b12f1c0cd0cddf46a1efe073e7
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-07-30 09:45:28 -0700

    Added something on login page

--- red-lantern/src/App.js
+++ red-lantern/src/App.js
@@ -1,12 +1,15 @@
 import HomeScreen from './pages/HomeScreen';
 import Navbar from './components/Navbar';
-import RestaurantDetails from './pages/RestaurantDetails';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Amplify, Auth } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { View, Image, useTheme, Text, Authenticator } from '@aws-amplify/ui-react';
 
-import config from './aws-exports';
-Amplify.configure(config)
+import awsExports from './aws-exports';
+Amplify.configure(awsExports)
 
 function App() {
   const handleSignOut = () => {
@@ -15,17 +18,49 @@ function App() {
       .catch(err => console.log('Error signing out:', err));
   };
 
+  const components = {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Contacts App"
+            src="/img/logo.png"
+          />
+        </View>
+      );
+    },
+    Footer() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; 2022 Jeremy Lehmann, Jeffrey Smith, Jasmina Topalovic, Syrinthia Swift, Aphra
+          </Text>
+        </View>
+      );
+    },
+  };
+
   return (
-    <div style={{ width: '100%' }}>
-
-      <Navbar signOut={handleSignOut} />
-
-      {/* <HomeScreen /> */}
-
-      <RestaurantDetails />
-
-    </div>
-  )
+    <Authenticator loginMechanisms={['email']} components={components}>
+      {({ signOut, user }) => (
+        <div>
+          <Navbar signOut={handleSignOut} />
+          <Router>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/register' element={<RegisterPage />} />
+            </Routes>
+          </Router>
+        </div>
+      )}
+    </Authenticator>
+  );
 }
 
-export default withAuthenticator(App);
+
+export default App;
