1e29ec4d2e4ac7d7859a528afe2bf0c50a03b263
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-08-03 21:08:27 -0700

    Cart doesn't reset when leaving the restaurant page

--- red-lantern/src/pages/cart.js
+++ red-lantern/src/pages/cart.js
@@ -2,27 +2,27 @@ import React from 'react';
 import { useCartContext } from '../contexts/CartContext';
 
 const Cart = () => {
-  const { cartItems } = useCartContext();
+    const { cartItems } = useCartContext();
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <h1>Order Summary</h1>
-          
-          <ul className="list-group">
-            {cartItems.map((item, index) => (
-              <li key={index} className="list-group-item">
-                <p>{item.quantity} {item.meal.name}</p>
-              </li>
-              
-            ))}
-          </ul>
-          <button class="btn btn-outline-danger" style={{ fontSize: '20px',  width: '180px', margin:'10px' }}>Add Items</button> <button class="btn btn-outline-danger" style={{ fontSize: '20px',  width: '180px', margin:'10px' }}>Place Order</button>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4">
+                    <h1>Order Summary</h1>
+
+                    <ul className="list-group">
+                        {cartItems.map((item, index) => (
+                            <li key={index} className="list-group-item">
+                                <p>{item.quantity} {item.meal.name}</p>
+                            </li>
+
+                        ))}
+                    </ul>
+                    <button class="btn btn-outline-danger" style={{ fontSize: '20px', width: '180px', margin: '10px' }}>Add Items</button> <button class="btn btn-outline-danger" style={{ fontSize: '20px', width: '180px', margin: '10px' }}>Place Order</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Cart;
