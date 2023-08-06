b7b3a0a21080b397c9cfbc437f681c70cad12a98
Jenay Swift <jenayswift@Jenays-MacBook-Pro.local>
2023-08-06 12:52:57 -0700

    update Navbar.css, App.js, Navbar.js

--- red-lantern/src/App.js
+++ red-lantern/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Amplify, Auth } from 'aws-amplify';
 import { View, Image, useTheme, Text, Authenticator } from '@aws-amplify/ui-react';
 import awsExports from './aws-exports';
@@ -22,7 +22,7 @@ Amplify.configure(awsExports);
 
 function App() {
   const [cartItemsCount, setCartItemsCount] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0);
   const { dbUser } = useAuthContext();
 
   // Sign out
@@ -85,7 +85,6 @@ function App() {
       </Authenticator>
       <Footer />
     </CartProvider>
-
   );
 }
 
